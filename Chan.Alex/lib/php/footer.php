91888122a9af637be1388735ddde4382ab28c569
Chan <alex.chan.0903@gmail.com>
2024-04-12 17:43:46 -0700

    Update file

---
+++ Chan.Alex/lib/php/footer.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="styleguide.css">
+    <style>
+         footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: #778C49;
+            color: #ffffff;
+            padding: 20px 0;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<footer>
+    <h3>&copy; 2024 EdenGrove All Rights Reserved.</h3>
+</footer>
+
+</body>
+</html>
---
+++ Chan.Alex/lib/php/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Info</title>
+    <link rel="stylesheet" href="styleguide.css">
+</head>
+<body>
+
+<header>
+    <h1 style="text-align: center;">Product Info</h1>
+</header>
+
+<div class="toc-container container">
+    <div class="section">
+        <h2><a href="products.sql">Products</a></h2>
+    </div>
+</div>
+
+</body>
+</html>
